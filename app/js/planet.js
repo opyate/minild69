71fd03efcca650a0c6ca08f58951f867d65c30a2
Juan M. Uys <opyate@gmail.com>
2016-07-13 12:47:51 +0100

    coloured faces

--- app/js/planet.js
+++ app/js/planet.js
@@ -4,9 +4,17 @@ FOO.addPlanet = function(scene, size, pos) {
     size = size || 200;
     pos = pos || 0;
     var geometry = new THREE.BoxGeometry(size, size, size);
+
+    var colors = _.times(6, function() { return new THREE.Color(Math.random() * 0xffffff); });
+
+    for (var i = 0; i < 6; i++) {
+        geometry.faces[2 * i].color = colors[i];
+        geometry.faces[2 * i + 1].color = colors[i];
+    }
+
     var material = new THREE.MeshBasicMaterial({
-        color: 0xff0000,
-        wireframe: true
+        color: 0xffffff,
+        vertexColors: THREE.FaceColors
     });
     var mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(pos,0,10);
