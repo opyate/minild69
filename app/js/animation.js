db45a12eca904c4af130d6d9c70315f3d04d4ad3
Juan M. Uys <opyate@gmail.com>
2016-07-13 12:56:36 +0100

    take planet initial orientation into account

--- app/js/animation.js
+++ app/js/animation.js
@@ -2,39 +2,45 @@ var FOO = FOO || {};
 
 FOO.animating = false;
 
+function getDeltaForDirection(direction) {
+    var state = {};
+    switch(direction) {
+    case 'left':
+        state = {
+            axis: 'y',
+            delta: 0 - Math.PI / 2
+        };
+        break;
+    case 'right':
+        state = {
+            axis: 'y',
+            delta: Math.PI / 2
+        };
+        break;
+    case 'up':
+        state = {
+            axis: 'x',
+            delta: 0 - Math.PI / 2
+        };
+        break;
+    case 'down':
+        state = {
+            axis: 'x',
+            delta: Math.PI / 2
+        };
+        break;
+    }
+    return state;
+}
+
 FOO.move = function(world, direction) {
     if (!FOO.animating) {
-        var state = {};
-        switch(direction) {
-        case 'left':
-            state = {
-                axis: 'y',
-                delta: Math.PI / 2
-            };
-            break;
-        case 'right':
-            state = {
-                axis: 'y',
-                delta: 0 - Math.PI / 2
-            };
-            break;
-        case 'up':
-            state = {
-                axis: 'x',
-                delta: Math.PI / 2
-            };
-            break;
-        case 'down':
-            state = {
-                axis: 'x',
-                delta: 0 - Math.PI / 2
-            };
-            break;
-        }
+        var state = getDeltaForDirection(direction);
 
-
-        new TWEEN.Tween({pos: 0})
-            .to({pos: state.delta}, 500)
+        var from = world.props.planet.rotation[state.axis];
+        var to = world.props.planet.rotation[state.axis] + state.delta;
+        new TWEEN.Tween({pos: from})
+            .to({pos: to}, 500)
             .onStart(function() {
                 FOO.animating = true;
             })
