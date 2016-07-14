2b28334a1e93dc091f988c965818686ec22f1b1f
Juan M. Uys <opyate@gmail.com>
2016-07-14 19:38:24 +0100

    extra colonist stencils

--- app/js/logic.js
+++ app/js/logic.js
@@ -5,10 +5,38 @@ var _ = require('lodash');
 var assert = require('assert');
 
 function getRandom() {
-    return Math.random();
+    return Math.random() > 0.5;
 }
 
-function getNbyN(n, fn) {
+function getHalf(n, i, j) {
+    return i < n / 2;
+}
+
+function getQuarter(n, i, j) {
+    return i < n / 2 ^ j > n / 2;
+}
+
+function getFull() {
+    return true;
+}
+
+function getCheckered(n, i, j) {
+    return (i + j) % 2 == 0;
+}
+
+function getDiagonal(n, i, j) {
+    return i + j == n;
+}
+
+function getCenter(n, i, j) {
+    return i == Math.floor(n/2) && j == Math.floor(n/2);
+}
+
+function getOne(n, i, j) {
+    return i == 1 && j == 1;
+}
+
+function getSquare(n, fn) {
     var arr = [];
     _.times(n, function(i) {
         _.times(n, function(j) {
@@ -16,7 +44,7 @@ function getNbyN(n, fn) {
                 arr[i] = [];
             }
             if (typeof fn === 'function') {
-                arr[i][j] = fn();
+                arr[i][j] = fn(n, i, j);
             } else {
                 arr[i][j] = fn;
             }
@@ -25,24 +53,22 @@ function getNbyN(n, fn) {
     return arr;
 }
 
-// Get 'm' faces of dim 'n' of getNbyN
+function invert(mtx) {
+    return _.map(mtx, function (row) {
+        return _.map(row, function (atom) {
+            return !atom;
+        });
+    });
+}
+
+// Get 'm' faces of dim 'n' of getSquare
 // all initialised to false.
 function faces(m, n, fn) {
     var arr = [];
     _.times(m, function(i) {
-        arr[i] = getNbyN(n, false);
+        arr[i] = getSquare(n, false);
     });
     return arr;
-}
-
-function flip(arr) {
-    return _.reduce(arr, function (acc, nestedArr) {
-        acc.push(_.reduce(nestedArr, function (nestedAcc, n) {
-            nestedAcc.push(n > 0.5);
-            return nestedAcc;
-        }, []));
-        return acc;
-    }, []);
 }
 
 function rot(matrix, n) {
@@ -60,7 +86,7 @@ function rot(matrix, n) {
 }
 
 // percentage col
-function perCol(plt) {
+function calculateColonised(plt) {
     return _.reduce(plt, function (accFace, face) {
         return _.reduce(face, function (accRow, row) {
             return _.reduce(row, function (acc, atom) {
@@ -87,14 +113,12 @@ function slam(plt, st, idx, rots) {
     });
 }
 
+var DIM = 5;
+var CDIM = 6; // number of faces on plt
 
 function test() {
-    var DIM = 5;
-    var nbyn = getNbyN(DIM, getRandom);
-    //console.log('nbyn', nbyn);
 
-    var st = flip(nbyn);
-    //console.log('st', st);
+    var st = getSquare(DIM, getRandom);
 
     // rotate 4 times for no effect.
     var r = rot(st, 4);
@@ -102,26 +126,37 @@ function test() {
     assert(_.isEqual(st, r));
     assert(_.isEqual(r, r2));
 
-    var CDIM = 6; // number of faces on plt
     var plt = faces(CDIM, DIM, false);
     //console.log('faces', plt);
-    //console.log('percol before', perCol(plt));
+    //console.log('percol before', calculateColonised(plt));
 
     //slam(plt, st, 3, 2);
     //console.log('faces', plt);
-    //console.log('percol after', perCol(plt));
+    //console.log('percol after', calculateColonised(plt));
 
     // full sim
     _.times(CDIM, function(idx) {
         // rots = 0, because it doesn't matter now...
-        slam(plt, flip(getNbyN(DIM, getRandom)), idx, 0);
+        slam(plt, getSquare(DIM, getRandom), idx, 0);
     });
     console.log(plt);
-    var result = perCol(plt);
+    var result = calculateColonised(plt);
     console.log(result);
     assert(result.on + result.off == CDIM * DIM * DIM);
 }
 
+function test2() {
+    var st = getSquare(DIM, getHalf);
+    //console.log('st', st);
+    //console.log('st inv', invert(st));
+
+    var st2 = getSquare(DIM, getOne);
+    console.log('st2', st2);
+    console.log('st2 inv', invert(st2));
+
+    var plt = faces(CDIM, DIM, false);
+}
 
 
-test();
+//test();
+test2();
