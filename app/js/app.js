d4011c3b6b7fd12af3adc7124b0bddab084eb8ec
Juan M. Uys <opyate@gmail.com>
2016-07-14 20:43:59 +0100

    not use global TWEEN

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,7 @@ define([
     'basics',
     'props',
     'animation'
-], function(THREE, _, tw, basics, props, anim) {
+], function(THREE, _, tween, basics, props, animation) {
 
     var init = function() {
         // set the stage (a scene, a camera, and a renderer...)
@@ -25,11 +25,11 @@ define([
 
         _.each(['a', 'd', 'w', 's', 'q', 'e'], function (direction) {
             if (world.keyboard.pressed(direction)) {
-                anim.move(world, direction);
+                animation.move(world, direction);
                 return false; // break
             }
         });
-        TWEEN.update();
+        tween.update();
 
         world.stage.renderer.render(world.stage.scene, world.stage.camera);
     };
